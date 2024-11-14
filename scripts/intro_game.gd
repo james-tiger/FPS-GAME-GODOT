6b79d91eae592540724c92ebe456de6721f8d87d
TIGER <52138474+james-tiger@users.noreply.github.com>
2024-11-15 01:57:04 +0500

    update

--- scripts/intro_game.gd
+++ scripts/intro_game.gd
@@ -1,7 +1,6 @@
 extends CanvasLayer
 
 @onready var animation_player: AnimationPlayer = $Control/AnimationPlayer
-@onready var music_player: AudioStreamPlayer3D = $Control/music_player
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
@@ -10,10 +9,7 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	elif anim_name == "Scene2":
 		animation_player.play("Scene3")
 	elif anim_name == "Scene3":
-		music_player.stop()  # Stop any currently playing music
-		music_player.play()   # Play the music for the world scene
-		get_tree().change_scene_to_file("res://scenes/menu.tscn")
+		get_tree().change_scene_to_file("res://menu.tscn")
 
 func _on_skips_pressed() -> void:
-	music_player.stop()  # Stop music when skipping
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	get_tree().change_scene_to_file("res://menu.tscn")
